68f397d44ca21c1c43d46dcc908656b2b6cbfeae
ferdyc <ferdy.ciputra@batmandiri.com>
2020-04-27 21:08:53 +0700

    initial webpack

---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+                /* rules buat global style */
+                test: /\.css$/,
+                include: /styles/,
+                use: [{
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            {
+                /* rules buat component */
+                test: /\.css$/i,
+                exclude: /styles/,
+                use: ["to-string-loader", "css-loader"]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        })
+    ]
+}
