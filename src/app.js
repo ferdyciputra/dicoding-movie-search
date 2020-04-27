3ead4a0c4e67fac3c64236b48989538b7c1de9b8
ferdyc <ferdy.ciputra@batmandiri.com>
2020-04-27 21:09:29 +0700

    add component search-bar

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import "./styles/style.css";
+import "../src/scripts/component/landing-page";
+import "../src/scripts/view/main";
---
+++ src/scripts/component/landing-page.js
@@ -0,0 +1,18 @@
+class LandingPage extends HTMLElement {
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4"><span>Search</span> your <br> favourite <span>Movie</span></h1>
+            </div>
+        </div>
+        `
+    }
+}
+
+customElements.define("landing-page", LandingPage);
