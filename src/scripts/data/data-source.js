ac128c40e5215a173e11cfbaaffdf6fa313680a1
ferdyc <ferdy.ciputra@batmandiri.com>
2020-04-29 20:20:47 +0700

    create datasorce from omdb api

---
+++ src/scripts/data/data-source.js
@@ -0,0 +1,20 @@
+class DataSource {
+    static async searchMovie(keyword) {
+        //Get Data Movie From API
+        const baseUrl = new URL('http://www.omdbapi.com/')
+
+        //set query params
+        baseUrl.search = new URLSearchParams({ apikey: 'c5a035b4', s: `${keyword}` });
+
+        // Send Response
+        const response = await fetch(baseUrl.toString());
+        const responseJson = await response.json();
+
+        if (responseJson.Search) {
+            return Promise.resolve(responseJson.Search);
+        } else {
+            return Promise.reject(responseJson.Error);
+        }
+    }
+}
+export default DataSource;
--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -1,1 +1,13 @@
 import "../component/search-bar";
+import DataSource from "../data/data-source";
+
+const main = async() => {
+    try {
+        const result = await DataSource.searchMovie('turtle')
+        console.log(result);
+    } catch (message) {
+        console.log(message)
+    }
+};
+
+main();
