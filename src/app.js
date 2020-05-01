df815f2230ba30d7f262c869f2b32f11ec88b060
ferdyc <ferdy.ciputra@batmandiri.com>
2020-05-01 14:25:29 +0700

    config webpack bootstrap

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+import "regenerator-runtime";
+import "bootstrap";
+import "./scss/app.scss";
 import "./styles/style.css";
 import "../src/scripts/component/landing-page";
+import "../src/scripts/component/modal-box";
 import swal from 'sweetalert';
 import main from "../src/scripts/view/main";
 
--- webpack.common.js
+++ webpack.common.js
@@ -28,6 +28,29 @@ module.exports = {
                 use: ["to-string-loader", "css-loader"]
             },
             {
+                test: /\.(scss)$/,
+                use: [{
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function() {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
                 test: /\.(gif|png|jpe?g|svg)$/i,
                 use: [
                     'file-loader',
